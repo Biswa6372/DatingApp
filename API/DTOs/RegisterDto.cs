372d6fe821b8aefa7b7b518eaa93e480533f1fcd
biswaranjan prusty <sprusty542@gmail.com>
2025-02-21 21:47:47 +0530

    End of Section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,7 @@ namespace API.DTOs;
 public class RegisterDto
 {
     [Required]
-    public required string UserName { get; set;}
+    public  string UserName { get; set;} = string.Empty;
     [Required]
-    public required string Password { get; set; }
+    public  string Password { get; set; } = string.Empty;
 }
