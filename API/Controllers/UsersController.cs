481362c293b9dde1b80be936378827929ab4cd66
biswaranjan prusty <sprusty542@gmail.com>
2025-03-31 18:25:29 +0530

    End of Section 16

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using API.Extensions;
 using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,7 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
      Response.AddPaginationHeader(users);
      return Ok(users);
   }
+
   [HttpGet("{username}")]
   public async Task<ActionResult<MemberDto>> GetUser(string username)
   {
---
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -0,0 +1,31 @@
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles-modal.component.html',
+  styleUrl: './roles-modal.component.css'
+})
+export class RolesModalComponent {
+  bsModalRef = inject(BsModalRef);
+  username = '';
+  title = '';
+  availableRoles: string[] = [];
+  selectedRoles: string[] = [];
+  rolesUpdated = false;
+
+  updateChecked(checkedValue: string){
+    if(this.selectedRoles.includes(checkedValue)){
+      this.selectedRoles = this.selectedRoles.filter(r => r !== checkedValue)
+    }else{
+      this.selectedRoles.push(checkedValue);
+    }
+  }
+
+  onSelectRoles(){
+    this.rolesUpdated=true;
+    this.bsModalRef.hide();
+  }
+}
